d6409e02036726b021c5014d05f9954eacbae223
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-04-29 23:42:18 +0000

    Assign tokens to variables

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -8,7 +8,18 @@ while True:
     user_input = input("Enter your equation > ")
     #tokenize user input
     tokens = user_input.split(" ")
-    
+
+
+    #assign variables to tokens    
+    operator = tokens[0]
+    num1 = tokens[1]
+
+    #factor in if there are > than 2 tokens
+    if len(tokens) == 3:
+        num2 = tokens[2]
+    if len(tokens) > 3:
+        num3 = tokens[3]
+        
     #create break if they want to quit
     #create break if they want to quit
     #assign variables to tokens  
