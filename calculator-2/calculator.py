1195df3a6e2f99d3a3d6a761579203aedba5eff0
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-04-29 23:33:17 +0000

    Created some Pseudocode to get started

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -3,4 +3,11 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
+# create a loop
+    #get user equation
+    #tokenize user input
+    #create break if they want to quit
+    #assign variables to tokens  
+    #factor in if there are > than 2 tokens
+    #pass arithmatic functions
+    #add float
