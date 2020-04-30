c428bd147f5c85b668e36bb48a9f920269f44574
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-04-30 05:17:27 +0000

    Deleted irrelivant comments

--- melon-delivery-report/produce_summary.py
+++ melon-delivery-report/produce_summary.py
@@ -1,33 +1,3 @@
-# print("Day 1")
-# the_file = open("um-deliveries-20140519.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
-
-#     melon = words[0]
-#     count = words[0]
-#     amount = words[0]
-
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()
-
-
-# print("Day 2")
-# the_file = open("um-deliveries-20140520.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
-
-#     melon = words[0]
-#     count = words[0]
-#     amount = words[0]
-
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()
-
-
 #Create function, set parameter
 def melon_delivery_report(date, the_file):
       
